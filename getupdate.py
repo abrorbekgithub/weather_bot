f91df588c053082410b67c643507eb05aa2a5840
abrorbek <abrorbekgithub@gmail.com>
2021-02-12 16:59:02 +0500

    getupdate function

---
+++ getupdate.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from pprint import pprint
+import os
+
+token=os.environ["TOKEN"]
+
+def getUpdates():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
+    res=requests.get(url)
+    updates=res.json()["result"]
+    return updates
+
+def last():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
+    res=requests.get(url)
+    updates=res.json()["result"]
+    return updates[-1]
+pprint(last())
+
+# ids_text=
+
+
+
+
+# city_name="Fergana"
+# api_key='1b4ba2abbe57cb51a299f2a1c620f80b'
+# url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+# res=requests.get(url)
+# data=res.json()
+# pprint(data)
+
